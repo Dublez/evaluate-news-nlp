f0c5bb50a410212f51de84a0f9ee2aff4751bccc
Nikita Ivanov <mybestjob@myself.com>
2021-02-13 18:36:46 +0300

    Added jest and updated readme

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,40 +1,46 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
+    let formText = getFormText();
     // check what text was put into the form field
-    let formText = document.getElementById('name').value;
     Client.checkForName(formText);
 
     console.log("::: Form Submitted :::");
-
-    const postServerData = async (url='', text) => {
-        // 
-        const body = JSON.stringify(text);
-        console.log(body);
-        const res = await fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({"input": text})
-        });
-        try {
-            const textAnalysisResponse = await res.json();
-            return textAnalysisResponse;
-        } catch (error) {
-            console.log('error', error);
-        }
-    }
 
     postServerData('http://localhost:8080/test', formText)
     // 
     // .then(res => console.log(res))
     // .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = "Confidence: "+ res.json.confidence;
-    });
+    .then((res) => renderResult(res));
         
+}
+
+function getFormText(){
+    return document.getElementById('name').value;
+}
+
+async function postServerData (url='', text) {
+    // 
+    const body = JSON.stringify(text);
+    console.log(body);
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({"input": text})
+    });
+    try {
+        const textAnalysisResponse = await res.json();
+        return textAnalysisResponse;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+function renderResult(res){
+    return document.getElementById('results').innerHTML = "Confidence: "+ res.json.confidence;
 }
 
 export { handleSubmit }
