2dfd9d177b4eb26ce208167948d2f0e9940aee54
Nikita Ivanov <mybestjob@myself.com>
2021-02-20 10:42:46 +0300

    changed path

--- src/server/index.js
+++ src/server/index.js
@@ -27,7 +27,7 @@ moment = require('moment');
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname+'/../client/views/' });
+    res.sendFile('index.html', { root: __dirname});
     // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
